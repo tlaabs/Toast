c88e627ba286bf0a41b95f40fd0c21f58d326a4c
KimYJin <95yjkim95@gmail.com>
2018-05-14 22:48:55 +0900

    menu icon at main

--- app/src/main/java/org/androidtown/toast_main/MainActivity.java
+++ app/src/main/java/org/androidtown/toast_main/MainActivity.java
@@ -2,6 +2,8 @@ package org.androidtown.toast_main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     *  main_menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int curId = item.getItemId();
+        switch (curId){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
