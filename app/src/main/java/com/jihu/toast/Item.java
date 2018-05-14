9ae12ddb30197de5af139f542fbeb2258237944b
DESKTOP-O2DPBHB\JiHu <paransky9577@gmail.com>
2018-05-14 16:10:44 +0900

    20180414 jihu db start

---
+++ app/src/main/java/com/jihu/toast/Item.java
@@ -0,0 +1,15 @@
+package com.jihu.toast;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by JiHu on 2018-05-14.
+ */
+
+public class Item extends SQLiteOpenHelper {
+
+    private Context context;
+
+    public Item
+}
